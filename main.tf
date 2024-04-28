e0fc08a6d7203d49f5b7b78693ab4cfb1ddb8794
Mohssen Sadeq <mohsen.alyafey@gmail.com>
2024-04-28 17:41:58 +0200

    add variables with best practices

--- main.tf
+++ main.tf
@@ -21,7 +21,8 @@
 
 provider "aws" {
   region = "us-east-1"
-profile = "default"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "access_key" {
+  description = "value of the AWS access key"
+  type = string
+}
+variable "secret_key" {
+  description = "value of the AWS secret key"
+    type = string
+}
